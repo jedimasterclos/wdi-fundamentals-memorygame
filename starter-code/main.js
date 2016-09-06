d434e8cd45dba90418d0fe6ce143c2af0040a225
Carlos S <Carlos@I-scream-at-deaf-people.local>
2016-09-06 16:41:50 -0700

    Added if, then

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,14 @@ var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
+
+if (cardOne === cardTwo) {
+		alert('Scored a match!');
+}	else {
+	alert('Please try again');
+}
+if (cardThree === cardFour) {
+		alert('Scored a match!');
+}	else {
+	alert('Please try again');
+}
