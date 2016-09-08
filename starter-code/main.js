689883d88d7ef4ba2d3650d5b21b7c73aa2850c1
Carlos S <Carlos@I-scream-at-deaf-people.local>
2016-09-07 23:38:02 -0700

    FinalCut

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,15 +1,42 @@
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 
 var board = document.getElementById('game-board');
 
 function createBoard() {
-	for (var i=0; i<cards.length; i++) {
+	for (var i=0; i < cards.length; i++) {
 		var cardElement = document.createElement('div');
 		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards [i]);
+		cardElement.addEventListener('click', isTwoCards);
 		board.appendChild(cardElement);
 	}
 }
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+		console.log(this.getAttribute('data-card'));
+		if (this.getAttribute('data-card') === 'king') {
+			this.innerHTML = "<img src='KingLl.jpg'>";
+		} else {
+			this.innerHTML = "<img src ='QueenCc.jpg'>";
+		}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+		for (var i = 0; i < cards.length; i++) {
+    		document.getElementsByClassName('card')[i].innerHTML = " ";
+  		}
+	}
+}
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+	alert ("Match Made!");
+	} else {
+	 alert("Please try again :(");
+	} 
+}
+
+createBoard();
